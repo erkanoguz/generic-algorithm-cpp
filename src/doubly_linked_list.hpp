889e08c1b9bb635a821e5f353b31354dd164dff8
erkanoguz <ooguzerkan@gmail.com>
2022-08-08 22:32:24 +0300

    iterator added

--- src/doubly_linked_list.hpp
+++ src/doubly_linked_list.hpp
@@ -3,6 +3,7 @@
 
 #include <list>
 #include <iostream>
+#include <vector>
 
 /**
  * TODO: add iterator capability
@@ -27,11 +28,148 @@ namespace detail
 } // namespace detail
 
 
+template <typename _Tp>
+struct _ListIterator 
+{
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = _Tp;
+    using pointer  = _Tp*;
+    using reference = _Tp&;
+
+    _ListIterator() noexcept { } 
+
+    explicit _ListIterator(detail::Node<_Tp>* node) noexcept 
+    : _node(node) { }
+
+    reference operator*() const noexcept 
+    {
+        return _node->_key;
+    }
+
+    pointer operator& () const noexcept
+    {
+        return &_node->key;
+    }
+
+    _ListIterator<_Tp>& operator++() noexcept 
+    {
+        _node = _node->_next;
+        return *this;
+    }
+
+    _ListIterator<_Tp> operator++(int) noexcept 
+    {
+        _ListIterator<_Tp> temp = *this;
+        _node = _node->_next;
+        return temp;
+    }
+
+    _ListIterator<_Tp>& operator--() noexcept 
+    {
+        _node = _node->prev;
+        return *this;
+    }
+
+    _ListIterator<_Tp> operator--(int) noexcept 
+    {
+        _ListIterator<_Tp> temp = *this;
+        _node = _node->_prev;
+        return temp;
+    }
+
+    friend bool operator== (const _ListIterator<_Tp>& lhs, const _ListIterator<_Tp>& rhs) noexcept 
+    {
+        return lhs._node == rhs._node;
+    } 
+
+    friend bool operator!= (const _ListIterator<_Tp>& lhs, const _ListIterator<_Tp>& rhs) noexcept 
+    {
+        return lhs._node != rhs._node;
+    } 
+
+    detail::Node<_Tp>* _node;
+};
+
+template <typename _Tp>
+struct _ListConstIterator 
+{
+    using iterator_category = std::bidirectional_iterator_tag;
+    using difference_type = std::ptrdiff_t;
+    using value_type = _Tp;
+    using pointer  = _Tp*;
+    using reference = _Tp&;  
+
+    _ListConstIterator() noexcept { }
+
+    explicit _ListConstIterator(const detail::Node<_Tp>* node) noexcept
+    : _node(node) { }
+
+    _ListConstIterator(const _ListConstIterator<T>& listIter) noexcept
+    : _node(listIter._node) { }
+
+    reference operator*() const noexcept 
+    {
+        return _node->_key;
+    }
+
+    pointer operator&() const noexcept 
+    {
+        return &_node->key;
+    }
+
+    _ListIterator<_Tp>& operator++() noexcept 
+    {
+        _node = _node->_next;
+        return *this;
+    }
+
+    _ListIterator<_Tp> operator++(int) noexcept 
+    {
+        _ListIterator<_Tp> temp = *this;
+        _node = _node->_next;
+        return temp;
+    }
+
+    _ListIterator<_Tp>& operator--() noexcept 
+    {
+        _node = _node->prev;
+        return *this;
+    }
+
+    _ListIterator<_Tp> operator--(int) noexcept 
+    {
+        _ListIterator<_Tp> temp = *this;
+        _node = _node->_prev;
+        return temp;
+    }
+
+    friend bool operator== (const _ListIterator<_Tp>& lhs, const _ListIterator<_Tp>& rhs) noexcept 
+    {
+        return lhs._node == rhs._node;
+    } 
+
+    friend bool operator!= (const _ListIterator<_Tp>& lhs, const _ListIterator<_Tp>& rhs) noexcept 
+    {
+        return lhs._node != rhs._node;
+    } 
+
+    detail::Node<_Tp>* _node;
+};
+
+
+
 template <typename T>
 class DoublyLinkedList
 {
     using Node_t = detail::Node<T>;
 public:
+    using value_type = T;
+    using iterator = _ListIterator<T>;
+    using const_iterator = _ListConstIterator<T>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+
     DoublyLinkedList()
         : _head(nullptr)
         , _tail(nullptr)
@@ -78,14 +216,57 @@ public:
         return *this;
     }
 
-    Node_t* search(T key) noexcept
+    // iterators
+    iterator begin() noexcept { return iterator(_head); }
+    const_iterator begin() const noexcept { return const_iterator(_head); }
+    iterator end() noexcept { return iterator(_tail); }
+    const_iterator end() const noexcept { return const_iterator(_tail); }
+    reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+    reverse_iterator rend() noexcept { return reverse_iterator(begin()); }
+    const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator(end()); }
+    const_reverse_iterator rend() const noexcept { return const_reverse_iterator(begin()); }
+    const_iterator cbegin() const noexcept { return const_iterator(_head); }
+    const_iterator cend() const noexcept { return const_iterator(_tail); }
+    const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator(end()); }
+    const_reverse_iterator crend() const noexcept { return const_reverse_iterator(begin()); }
+
+    [[nodiscard]] bool empty() const noexcept
     {
-        return _do_search(key);
+        if (_head == nullptr) return true;
+
+        return false;
     }
 
-    void push_front(T key) noexcept
+    std::size_t size() const noexcept 
     {
-        Node_t* newNode = new Node_t(std::move(key));
+        return _nodeCount;
+    }
+
+    // Element access
+    T& front() noexcept 
+    { 
+        return *begin(); 
+    }
+    
+    const T& front() const noexcept 
+    { 
+        return *begin(); 
+    }
+
+    T& back() noexcept
+    {
+        return *end();
+    }
+
+    const T& back() const noexcept 
+    {
+        return *end();
+    }
+    
+
+    // Modifiers
+    void push_front(const value_type& key) noexcept
+    {
         newNode->_next = _head;
         if (_head != nullptr) {
             _head->_prev = newNode;
@@ -93,6 +274,25 @@ public:
             _tail = newNode;
         }
         _head = newNode;
+    }
+
+    Node_t* _create_node(const value_type& key) 
+    {
+        Node_t* node = new Node_t(std::move(key));
+        return node;
+    }
+
+    void _insert(Node_t* node, const value_type& key) noexcept
+    {
+        Node_t* temp = _create_node(key);
+
+
+        _listPtr
+    }
+
+    Node_t* search(T key) noexcept
+    {
+        return _do_search(key);
     }
 
     void push_back(T key) noexcept
@@ -150,12 +350,6 @@ public:
         (push_front(std::forward<Args>(args)), ...);
     }
 
-    [[nodiscard]] bool empty() const noexcept
-    {
-        if (_head == nullptr) return true;
-
-        return false;
-    }
 
     void print() noexcept
     {
@@ -203,10 +397,9 @@ private:
 
     Node_t* _head;
     Node_t* _tail;
+
+    Node_t* _listPtr;
 };
-
-
-
 
 } // namepscae exo
 
